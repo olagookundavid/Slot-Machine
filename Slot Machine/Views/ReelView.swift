8bcf80f1c99bc991be1bdee0774016c0241087dd
David Olagookun <erijesudo@gmail.com>
2023-06-09 06:19:40 +0100

    Initial Commit

---
+++ Slot Machine/Views/ReelView.swift
@@ -0,0 +1,20 @@
+//
+//  Created by David OH
+//
+
+import SwiftUI
+
+struct ReelView: View {
+  var body: some View {
+    Image("gfx-reel")
+      .resizable()
+      .modifier(ImageModifier())
+  }
+}
+
+struct ReelView_Previews: PreviewProvider {
+  static var previews: some View {
+    ReelView()
+      .previewLayout(.fixed(width: 220, height: 220))
+  }
+}
